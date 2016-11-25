8ff409e1a25a47f5a4248dd77ddfccb68288bd2c
MarioCh125 <mario.ch.25@gmail.com>
2016-11-25 00:43:14 -0500

    ja

--- js/principal.js
+++ js/principal.js
@@ -3,7 +3,7 @@ var lar = screen.width;
 var alt = screen.height-20;
 var juego = new Phaser.Game(lar,alt,Phaser.CANVAS,'bloque_juego');
 juego.state.add('Menu',Menu);
-//juego.state.add('Nivel',Nivel);
+juego.state.add('Nivel',Nivel);
 juego.state.add('Juego1_1',Juego1_1);
 juego.state.add('Juego1_2',Juego1_2);
 //juego.state.add('Game_Over',Game_Over);
