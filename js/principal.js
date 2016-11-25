5f9fc9b7ff79330c75d52c9188a9163d95bbf3cc
DESKTOP-RE7LQ6C\mario <mario.ch.25@gmail.com>
2016-11-25 06:25:19 -0500

    pro
    probando

--- js/principal.js
+++ js/principal.js
@@ -4,7 +4,7 @@ var alt = screen.height-20;
 var juego = new Phaser.Game(lar,alt,Phaser.CANVAS,'bloque_juego');
 juego.state.add('Menu',Menu);
 juego.state.add('Nivel',Nivel);
-//juego.state.add('Juego1_1',Juego1_1);
+juego.state.add('Juego1_1',Juego1_1);
 juego.state.add('Juego1_2',Juego1_2);
 //juego.state.add('Game_Over',Game_Over);
 juego.state.start('Menu');
